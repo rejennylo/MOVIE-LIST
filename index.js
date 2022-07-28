4dbc759b840e2e379f67cdb797b2e1ed0b2aab97
rejennylo <rejennylo@gmail.com>
2022-07-28 11:51:12 +0800

    initialize project

---
+++ index.js
@@ -0,0 +1,64 @@
+const BASE_URL = 'https://movie-list.alphacamp.io'
+const INDEX_URL = BASE_URL + '/api/v1/movies/'
+const POSTER_URL = BASE_URL + '/posters/'
+
+const movies = []
+
+const dataPanel = document.querySelector('#data-panel')
+
+function renderMovieList(data) {
+  let rawHTML= ''
+
+  data.forEach((item) => {
+    //title, image
+    rawHTML += `<div class="col-sm-3">
+    <div class="mb-2">
+      <div class="card">
+        <img src="${POSTER_URL + item.image}" class="card-img-top" alt="Movie Poster">
+        <div class="card-body">
+          <h5 class="card-title">${item.title}</h5>
+        </div>
+        <div class="card-footer text-muted">
+          <button class="btn btn-primary btn-show-movie" data-bs-toggle="modal" data-bs-target="#movie-modal" data-id="${item.id}">More</button>
+          <button class="btn btn-info btn-show-favorite">+</button>
+        </div>
+      </div>
+    </div>
+  </div>`
+  })
+
+  dataPanel.innerHTML = rawHTML
+}
+
+function showMovieModal (id) {
+  const modalTitle = document.querySelector('#movie-modal-title')
+  const modalImage = document.querySelector('#movie-modal-image')
+  const modalDate = document.querySelector('#movie-modal-date')
+  const modalDescription = document.querySelector('#movie-modal-description')
+
+  axios.get(INDEX_URL + id)
+  .then((response) => {
+    const data = response.data.results
+    
+    modalTitle.innerText = data.title
+    modalImage.innerHTML = `<img src="${POSTER_URL + data.image}" alt="movie-poster" class="=img-fuid">`
+    modalDate.innerText = 'Release date: ' + data.release_date
+    modalDescription.innerText = data.description
+    })
+
+  .catch((error) => console.log(error))
+}
+
+dataPanel.addEventListener('click', function onPanelClicked(event) {
+  if (event.target.matches('.btn-show-movie')){
+    console.log(event.target.dataset)
+    showMovieModal(Number(event.target.dataset.id))
+  }
+})
+
+axios.get(INDEX_URL)
+.then((response) => {
+  movies.push(...response.data.results)
+  renderMovieList(movies)
+})
+.catch((error) => console.log(error))
